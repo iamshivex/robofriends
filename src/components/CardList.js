1212425677ef844b991e82d8dbd6ba2c7959bc1f
Oladejo Sherifu <sherifuoladejo@gmail.com>
2022-08-09 04:14:03 +0100

    sending robofriends to git

---
+++ src/components/CardList.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "./Card";
+
+// option one
+const CardList = ({ robots }) => {
+    const cardsArray = robots.map((user, i) => {
+        return <Card 
+            key={i} 
+            id={robots[i].id} 
+            name={robots[i].name} 
+            email={robots[i].email}
+        />
+    })
+
+    return (
+        <>
+            {cardsArray}
+        </>
+    )
+}
+
+export default CardList
+
+// option two but complex for now
+// const CardList = ({ robots }) => {
+//     return (
+//         <>
+//             {
+//                 robots.map((user, i) => {
+//                     return (
+//                         <Card 
+//                             key={i} 
+//                             id={robots[i].id} 
+//                             name={robots[i].name} 
+//                             email={robots[i].email}
+//                         />
+//                     )
+//                 })
+//             }
+//         </>
+//     );
+// }
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 100,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 101,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 102,
+    name: 'Clemtine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 103,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 104,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 105,
+    name: 'Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 106,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 107,
+    name: 'Nicholas Runolfs',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 108,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 109,
+    name: 'Clemena DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
