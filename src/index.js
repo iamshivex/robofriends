7595c02eec237e50dcd6fb87942afe7459b7b40e
Oladejo Sherifu <sherifuoladejo@gmail.com>
2022-09-25 07:03:41 +0100

    hook changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
-import "tachyons"
 import App from "./containers/App"
+import './index.css';
+import "tachyons";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
